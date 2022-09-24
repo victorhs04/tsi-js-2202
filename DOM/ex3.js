6de886b4a494f8dadecbb151a05b58e682b4a45a
VICTOR HUGO SOUZA OLIVEIRA <victor.hsoliveira15@senacsp.edu.br>
2022-09-23 22:05:59 -0300

    aula/23-09-2022

---
+++ DOM/ex3.js
@@ -0,0 +1,13 @@
+let e;
+//e = document.body.firstElementChild;
+//e = document.doctype
+//e = document.forms
+//e = document.forms[0]
+//e = document.forms[0][1];
+//e = document.scripts;
+//e = document.scripts[1];
+//e = document.scripts[2].getAttribute('src');
+//e = document.querySelector('li');
+//e = document.querySelector('div .row');
+//e = document.querySelector('input #task');
+console.log(e);
