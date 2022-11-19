6f88f6e88e642e08be3e25681aea488e2652e51e
VICTOR HUGO SOUZA OLIVEIRA <victor.hsoliveira15@senacsp.edu.br>
2022-11-18 22:06:52 -0300

    aula/18-11-2022

--- graficos/ipca.js
+++ graficos/ipca.js
@@ -1,85 +1,49 @@
-document.querySelector('button').addEventListener('click', mostrarIpca);
+document.querySelector('button').addEventListener('click', mostraIpca);
 
+function mostraIpca(){
+    document.getElementById('ipca').innerHTML = "";
+    
+    let anoEscolhido = document.getElementById('EscolhaAno').value;
 
-
-function mostrarIpca(evento){
-
-    evento.preventDefault();
-
-
-
-    const url ='api.bcb.gov.br/dados/serie/bcdata.sgs.4448/dados?formato=json&dataInicial=20201101'
-
-
+    const url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4448/dados?formato=json&dataInicial=20201101';
 
     fetch(url).then(function(retorno){
-
-
-
         return retorno.text();
-
     }).then(function(stringJson){
+        const indices = JSON.parse(stringJson);
 
+        let indice;
+        let ipca = [];
 
+        indices.forEach(function(mes){
+            anoString = mes.data.substring(6, 10);
+            mesString = mes.data.substring(3, 5);
+            anoMesString = anoString + '-' + mesString;
 
-        console.log(stringJson);
+            indice = parseFloat(mes.valor);
 
+            if(parseInt(anoString) != anoEscolhido && anoEscolhido != ""){
+                return;
+            }
 
+            ipca.push({month: anoMesString, value: indice});
 
-        jsonParaMorris = {
+        });
 
-
-
+        if(ipca.length == 0){
+            alert(`Não ha dados para o ano de ${anoEscolhido}`)
+        }
+        
+        new Morris.Line({
             element: 'ipca',
 
-            
+            data: ipca,
 
-            data: [
-
-              { year: '2008', value: 20 },
-
-              { year: '2009', value: 10 },
-
-              { year: '2010', value: 5 },
-
-              { year: '2011', value: 5 },
-
-              { year: '2013', value: 30 },
-
-              { year: '2014', value: 50 }
-
-            ],
-
- 
-
-            xkey: 'year',
-
-
-
+            xkey: 'month',
             ykeys: ['value'],
-
-            
-
-            labels: ['Value']
-
-
-
-        }
-
-
-
+            labels: ['IPCA']
+        });
     }).catch(function(){
-
-
-
-        alert('API do banco central fora do ar');
-
-    });
-
-
-
+        alert('API do Banco Central esta fora do ar');
+    })
 }
-
-
-
-
